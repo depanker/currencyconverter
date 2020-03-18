ebaf3d110a61f48ebfb0d40f9c372d20f599d3e7
depanker <depanker@gmail.com>
2020-03-18 22:02:05 +0530

    Added blockhound

--- src/main/java/com/depanker/currencyconverter/CurrencyconverterApplication.java
+++ src/main/java/com/depanker/currencyconverter/CurrencyconverterApplication.java
@@ -2,11 +2,18 @@ package com.depanker.currencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class CurrencyconverterApplication {
 
 	public static void main(String[] args) {
+		BlockHound.builder()
+				.allowBlockingCallsInside("sun.security.ssl.SSLHandshake", "consume")
+				.allowBlockingCallsInside("io.netty.util.concurrent.GlobalEventExecutor", "addTask")
+				.install();
+		Hooks.onOperatorDebug();
 		SpringApplication.run(CurrencyconverterApplication.class, args);
 	}
 
